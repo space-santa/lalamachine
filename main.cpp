a20a49909187656e7d3ffabdab7969dfa2e12885
Claus Zirkel <claus.z@protonmail.com>
2015-01-30 19:08:08 +1100

    Adding the app-icon.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <QApplication>
 #include <QQmlApplicationEngine>
+#include <QPixmap>
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    // Setting the app-icon.
+    app.setWindowIcon(QIcon(QPixmap(":/images/images/lalamachine.png")));
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
