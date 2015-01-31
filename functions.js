da3f74004116119beb097172c49e25770243fa55
Claus Zirkel <claus.z@protonmail.com>
2015-01-31 13:32:31 +1100

    Added a function to convert seconds to mm:ss string.

--- functions.js
+++ functions.js
@@ -5,8 +5,12 @@ function millisToSec(millis) {
 }
 
 function millisToMinSec(millis) {
-    var min = Math.floor(millis / 1000 / 60).toString()
-    var rem = Math.floor(millis / 1000 % 60).toString()
+    return secToMinSec(millisToSec(millis))
+}
+
+function secToMinSec(sec) {
+    var min = Math.floor(sec / 60).toString()
+    var rem = Math.floor(sec % 60).toString()
     rem = rem.length === 1 ? "0" + rem : rem
     return min + ":" + rem
 }
