d1c0545a675a4fcabdbc4648b359529bbc87f93b
Claus Zirkel <claus.z@protonmail.com>
2015-02-25 19:46:29 +1100

    * Added properties to get a list of genres, artists or albums. * Moved function definitions into the .cpp file. * Autoformatted.

--- musiclib.cpp
+++ musiclib.cpp
@@ -70,14 +70,29 @@ MusicLib::MusicLib(QQuickItem *parent)
     connect(scanner_, &MusicLibScanner::scanComplete, this,
             &MusicLib::scanFinished);
     connect(this, &MusicLib::musicLibChanged, this, &MusicLib::writeLibFile);
-    scannerThread_.start();
+    connect(this, &MusicLib::displayLibChanged,
+            this, &MusicLib::genreListChanged);
+    connect(this, &MusicLib::displayLibChanged,
+            this, &MusicLib::artistListChanged);
+    connect(this, &MusicLib::displayLibChanged,
+            this, &MusicLib::albumListChanged);
+    readLibFile();
+
+    if (lib_.isEmpty()) {
+        scannerThread_.start();
+    }
 }
 
 MusicLib::~MusicLib()
 {
     scannerThread_.quit();
-    scannerThread_.wait();
+    scannerThread_.wait(5000);
     qDebug() << "MusicLib::~MusicLib()" << displayLib();
+}
+
+QJsonObject MusicLib::musicLib() const
+{
+    return lib_;
 }
 
 void MusicLib::readLibFile()
@@ -99,16 +114,126 @@ QJsonObject MusicLib::displayLib() const
         if (itr.value().isObject()) {
             QJsonObject tmp = itr.value().toObject();
 
-            if (checkVal(genre(), tmp.value("genre").toString()) &&
-                checkVal(artist(), tmp.value("artist").toString()) &&
-                checkVal(album(), tmp.value("album").toString()))
-            {
+            if (checkVal(genreFilter(), tmp.value("genre").toString()) &&
+                checkVal(artistFilter(), tmp.value("artist").toString()) &&
+                checkVal(albumFilter(), tmp.value("album").toString())) {
                 retVal.insert(itr.key(), tmp);
             }
         }
     }
 
     return retVal;
+}
+
+QString MusicLib::libPath() const
+{
+    return libPath_;
+}
+
+void MusicLib::setLibPath(const QString &path)
+{
+    libPath_ = path;
+    emit libPathChanged();
+    emit startScan(libPath_);
+}
+
+QString MusicLib::genreFilter() const
+{
+    return genreFilter_;
+}
+
+void MusicLib::setGenreFilter(const QString &val)
+{
+    genreFilter_ = val;
+    emit genreFilterChanged();
+    emit displayLibChanged();
+}
+
+QString MusicLib::artistFilter() const
+{
+    return artistFilter_;
+}
+
+void MusicLib::setArtistFilter(const QString &val)
+{
+    artistFilter_ = val;
+    emit artistFilterChanged();
+    emit displayLibChanged();
+}
+
+QString MusicLib::albumFilter() const
+{
+    return albumFilter_;
+}
+
+void MusicLib::setAlbumFilter(const QString &val)
+{
+    genreFilter_ = val;
+    emit albumFilterChanged();
+    emit displayLibChanged();
+}
+
+QStringList MusicLib::getList(const QString &what) const
+{
+    Q_ASSERT(what == "genre" || what == "artist" || what == "album");
+    QStringList retVal;
+    QJsonObject tmplib = displayLib();
+    QJsonObject::const_iterator itr;
+
+    for (itr = tmplib.constBegin(); itr != tmplib.constEnd(); ++itr) {
+        if (itr.value().isObject()) {
+            QJsonObject tmpo = itr.value().toObject();
+            QString tmps = tmpo.value(what).toString();
+
+            if (!retVal.contains(tmps)) {
+                retVal << tmps;
+            }
+        }
+    }
+
+    return retVal;
+}
+
+QStringList MusicLib::genreList() const
+{
+    QStringList tmp;
+
+    if (genreFilter() != "") {
+        tmp << genreFilter();
+    }
+    else {
+        tmp = getList(QString("genre"));
+    }
+
+    return tmp;
+}
+
+QStringList MusicLib::artistList() const
+{
+    QStringList tmp;
+
+    if (artistFilter() != "") {
+        tmp << artistFilter();
+    }
+    else {
+        tmp = getList(QString("artist"));
+    }
+
+    return tmp;
+}
+
+QStringList MusicLib::albumList() const
+{
+    QStringList tmp;
+
+    if (albumFilter() != "") {
+        tmp << albumFilter();
+    }
+    else {
+        tmp = getList(QString("album"));
+    }
+
+    return tmp;
 }
 
 void MusicLib::scanFinished(const QJsonObject &lib)
--- musiclib.h
+++ musiclib.h
@@ -69,45 +69,25 @@ public:
     MusicLib(QQuickItem *parent = 0);
     ~MusicLib();
 
-    QJsonObject musicLib() const {
-        return lib_;
-    }
+    QJsonObject musicLib() const;
 
     QJsonObject displayLib() const;
 
-    QString libPath() const {
-        return libPath_;
-    }
-    void setLibPath(const QString &path) {
-        libPath_ = path;
-        emit libPathChanged();
-        emit startScan(libPath_);
-    }
+    QString libPath() const;
+    void setLibPath(const QString &path);
 
-    QString genre() const {
-        return genre_;
-    }
-    void setGenre(const QString &val) {
-        genre_ = val;
-        emit genreChanged();
-        emit displayLibChanged();
-    }
-    QString artist() const {
-        return artist_;
-    }
-    void setArtist(const QString &val) {
-        artist_ = val;
-        emit artistChanged();
-        emit displayLibChanged();
-    }
-    QString album() const {
-        return album_;
-    }
-    void setAlbum(const QString &val) {
-        genre_ = val;
-        emit albumChanged();
-        emit displayLibChanged();
-    }
+    QString genreFilter() const;
+    void setGenreFilter(const QString &val);
+
+    QString artistFilter() const;
+    void setArtistFilter(const QString &val);
+
+    QString albumFilter() const;
+    void setAlbumFilter(const QString &val);
+
+    QStringList genreList() const;
+    QStringList artistList() const;
+    QStringList albumList() const;
 
 public slots:
     void scanFinished(const QJsonObject &lib);
@@ -119,25 +99,36 @@ signals:
     void displayLibChanged();
 
     void libPathChanged();
-    void genreChanged();
-    void artistChanged();
-    void albumChanged();
+    void genreFilterChanged();
+    void artistFilterChanged();
+    void albumFilterChanged();
+
+    void genreListChanged();
+    void artistListChanged();
+    void albumListChanged();
+
+    void displayListChanged();
 
 private:
     // scanner_ MUST be a raw pointer. When this is moved to a new thread, that
     // QThread becomes the parent. When the parent dies so does the child.
     // If this is then not a raw pointer a double free happens, because
     // the thread and this is trying to destroy the scanner.
-    MusicLibScanner *scanner_{new MusicLibScanner()};
+    MusicLibScanner *scanner_ {new MusicLibScanner()};
     QThread scannerThread_;
 
     QJsonObject lib_;
-    QString genre_{""};
-    QString artist_{""};
-    QString album_{""};
-    QString libPath_{""};
+    QString genreFilter_ {""};
+    QString artistFilter_ {""};
+    QString albumFilter_ {""};
+    QString libPath_ {""};
+    QStringList genreList_ {};
+    QStringList artistList_ {};
+    QStringList albumList_ {};
 
     bool checkVal(const QString &check, const QString &val) const;
+
+    QStringList getList(const QString &what) const;
 
 private slots:
     void readLibFile();
