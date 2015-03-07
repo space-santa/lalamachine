6a06766303aa4dffb7a3723070a9a549407ca3c3
Claus Zirkel <claus.z@protonmail.com>
2015-03-08 08:28:29 +1100

    Q_ASSERT really must be QVERIFY in the test.

--- test/lalatest/tst_lalatesttest.cpp
+++ test/lalatest/tst_lalatesttest.cpp
@@ -40,23 +40,23 @@ void LalatestTest::timeTest() {
     TimeConverter tc;
     // Initialization
     testString = "00:00";
-    Q_ASSERT(tc.toString() == testString);
+    QVERIFY(tc.toString() == testString);
     // one minute
     testString = "01:00";
     tc.setSeconds(60);
-    Q_ASSERT(tc.toString() == testString);
+    QVERIFY(tc.toString() == testString);
     // one hour
     testString = "01:00:00";
     tc.setSeconds(3600);
-    Q_ASSERT(tc.toString() == testString);
+    QVERIFY(tc.toString() == testString);
     // one day
     testString = "1:00:00:00";
     tc.setSeconds(24 * 3600);
-    Q_ASSERT(tc.toString() == testString);
+    QVERIFY(tc.toString() == testString);
     // one day, one minute and 23 seconds
     testString = "1:00:01:23";
     tc.setSeconds(24 * 3600 + 60 + 23);
-    Q_ASSERT(tc.toString() == testString);
+    QVERIFY(tc.toString() == testString);
     // 12 days, 22 hours, thirtyone minutes and 23 seconds
     testString = "12:22:31:23";
     tc.setSeconds(12 *24 * 3600 + 22 * 3600 + 31 * 60 + 23);
