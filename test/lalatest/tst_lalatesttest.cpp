7935f8c2a0e0e8dda828238d5eeb4c4ec462fde3
Claus Zirkel <claus.z@protonmail.com>
2015-03-09 20:47:12 +1100

    Added unit test for m3uinout

--- test/lalatest/tst_lalatesttest.cpp
+++ test/lalatest/tst_lalatesttest.cpp
@@ -5,6 +5,8 @@
 
 #include "../../timeconverter.h"
 #include "../../systeminteractor.h"
+#include "../../m3uinout.h"
+#include "../../config.h"
 
 class LalatestTest : public QObject
 {
@@ -19,6 +21,7 @@ private Q_SLOTS:
     void testCase1();
     void timeTest();
     void systemInteractor();
+    void m3uInOut();
 };
 
 LalatestTest::LalatestTest()
@@ -38,7 +41,8 @@ void LalatestTest::testCase1()
     QVERIFY2(true, "Failure");
 }
 
-void LalatestTest::timeTest() {
+void LalatestTest::timeTest()
+{
     QString testString("");
     TimeConverter tc;
     // Initialization
@@ -62,7 +66,7 @@ void LalatestTest::timeTest() {
     QVERIFY(tc.toString() == testString);
     // 12 days, 22 hours, thirtyone minutes and 23 seconds
     testString = "12:22:31:23";
-    tc.setSeconds(12 *24 * 3600 + 22 * 3600 + 31 * 60 + 23);
+    tc.setSeconds(12 * 24 * 3600 + 22 * 3600 + 31 * 60 + 23);
     QVERIFY(tc.toString() == testString);
 }
 
@@ -77,6 +81,31 @@ void LalatestTest::systemInteractor()
     QVERIFY(sysInt.startDetached(QString("echo"), QStringList()));
     // Now it will be false since we can't successfully run nonexisting stuff.
     QVERIFY(!sysInt.startDetached(QString("gugulala25341"), QStringList()));
+}
+
+void LalatestTest::m3uInOut()
+{
+    M3uInOut m3;
+    QString laladir(Config::PLAYLISTDIR);
+    QString name("testlist456723");
+    QStringList testlist(QStringList() << "lala.mp3"
+                         << "gugu.mp3" << "dada.mp3"
+                         << "pupu.mp3");
+    QStringList returnList;
+
+    for (int i = 0; i < testlist.length(); ++i) {
+        returnList << "file://" + testlist[i];
+    }
+
+    QVERIFY(m3.m3uPath(name) == QString(laladir + "/" + name + ".m3u"));
+    m3.writePlaylist(name, testlist);
+    QVERIFY(m3.getPlaylistNames().contains(name));
+    QVERIFY(m3.readPlaylist(name) == returnList);
+    m3.deletePlaylist(name);
+    QVERIFY(!m3.getPlaylistNames().contains(name));
+    // A nonexisting playlistname will return an empty list.
+    QVERIFY(m3.readPlaylist("strangeunlikelyplaylistnamethatdoesntexist")
+            == QStringList());
 }
 
 QTEST_MAIN(LalatestTest)
