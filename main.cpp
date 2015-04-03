8b03057644c98500d927b9337fa7b7b5e1b08810
Claus Zirkel <claus.z@protonmail.com>
2015-04-03 19:27:24 +1100

    Set the version number in main.cpp to 0.11

--- main.cpp
+++ main.cpp
@@ -36,7 +36,7 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     // Setting the app-icon.
     app.setWindowIcon(QIcon(QPixmap(":/images/images/lalamachine.png")));
-    app.setApplicationVersion("0.10");
+    app.setApplicationVersion("0.11");
     app.setApplicationName("lalamachine");
 
     qmlRegisterType<MetaDataProvider>("Lala", 1, 0, "Metadata");
