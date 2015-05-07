68c0809297454b88ab069e1e6edafda1da53d7db
Claus Zirkel <claus.z@protonmail.com>
2015-05-07 21:29:43 +1000

    Removed the check for equality. As it turns out, the reset button is not working properly otherwise.

--- musiclib.cpp
+++ musiclib.cpp
@@ -572,13 +572,6 @@ void MusicLib::setCanSetDisplayLib(bool canSetDisplayLib)
 QString MusicLib::titlePartialFilter() const { return titlePartialFilter_; }
 void MusicLib::setTitlePartialFilter(const QString &titlePartialFilter)
 {
-    // If there is no change we don't want to emit the changed signal because
-    // we don't want to setDisplayLib if there is no change.
-    if (titlePartialFilter_ == titlePartialFilter) {
-        qDebug() << "No change, returning...";
-        return;
-    }
-
     titlePartialFilter_ = titlePartialFilter;
     emit titlePartialFilterChanged();
 }
