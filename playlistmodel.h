0305711b7bb2aa55ecdf928389074bb35cd56352
Claus Zirkel <claus.z@protonmail.com>
2015-07-23 20:58:19 +1000

    Using the same role as default as in the docs.
    --HG--
    branch : bugfix/list-sorting

--- playlistmodel.h
+++ playlistmodel.h
@@ -33,7 +33,7 @@ public:
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
     bool setData(const QModelIndex &index,
                  const QVariant &value,
-                 int role = TRACK);
+                 int role = Qt::EditRole);
     Qt::ItemFlags flags(const QModelIndex &index) const;
 
     void append(Track track);
