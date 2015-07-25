4b79d6a81a121811079ffa18a302d3d70bf3bf8e
Claus Zirkel <claus.z@protonmail.com>
2015-07-25 17:18:20 +1000

    Added some plank lines for better readability.
    --HG--
    branch : bugfix/list-sorting

--- playlistmodel.h
+++ playlistmodel.h
@@ -51,24 +51,32 @@ private:
     QList<Track> list_;
 
     void append(Track track);
+
     static bool sortTrackAsc(Track t1, Track t2);
     static bool sortTrackDesc(Track t1, Track t2);
+
     static bool sortTitleAsc(Track t1, Track t2);
     static bool sortTitleDesc(Track t1, Track t2);
+
     static bool sortGenreAsc(Track t1, Track t2);
     static bool sortGenreDesc(Track t1, Track t2);
+
     static bool sortArtistAsc(Track t1, Track t2);
     static bool sortArtistDesc(Track t1, Track t2);
+
     static bool sortAlbumAsc(Track t1, Track t2);
     static bool sortAlbumDesc(Track t1, Track t2);
+
     static bool sortCommentAsc(Track t1, Track t2);
     static bool sortCommentDesc(Track t1, Track t2);
     static int compareComment(QString c1, QString c2);
 
     static bool sortLengthAsc(Track t1, Track t2);
     static bool sortLengthDesc(Track t1, Track t2);
+
     static bool sortYearAsc(Track t1, Track t2);
     static bool sortYearDesc(Track t1, Track t2);
+
     static bool sortDateAddedAsc(Track t1, Track t2);
     static bool sortDateAddedDesc(Track t1, Track t2);
 };
