869fa3fefc091c1d661c8232c06a20899369b22c
Claus Zirkel <claus.z@protonmail.com>
2016-06-20 09:17:56 +1000

    Replaced include guard with pragma once.

--- src/track.h
+++ src/track.h
@@ -1,5 +1,4 @@
-#ifndef TRACK_H
-#define TRACK_H
+#pragma once
 
 #include <QString>
 #include <QJsonObject>
@@ -40,5 +39,3 @@ public:
     QString dateAdded_;
     int id_;
 };
-
-#endif  // TRACK_H
