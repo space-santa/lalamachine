21041de0172cbf2c3a3de6f0c66f4a6134a8be93
claus <claus.z@protonmail.com>
2017-11-25 22:16:53 +1100

    Apply the updated .clang-format to volumecontrol

--- src/view/volumecontrol.h
+++ src/view/volumecontrol.h
@@ -5,33 +5,33 @@
 #include <QWidget>
 
 class VolumeControl : public QWidget {
-  Q_OBJECT
- public:
-  VolumeControl(QWidget *parent);
+    Q_OBJECT
+public:
+    VolumeControl(QWidget *parent);
 
-  Q_PROPERTY(bool muted READ getMuted WRITE setMuted NOTIFY mutedChanged);
-  Q_PROPERTY(int volume READ getVolume WRITE setVolume NOTIFY volumeChanged);
+    Q_PROPERTY(bool muted READ getMuted WRITE setMuted NOTIFY mutedChanged);
+    Q_PROPERTY(int volume READ getVolume WRITE setVolume NOTIFY volumeChanged);
 
-  bool getMuted() const;
-  void setMuted(bool muted);
+    bool getMuted() const;
+    void setMuted(bool muted);
 
-  int getVolume() const;
-  void setVolume(int volume);
+    int getVolume() const;
+    void setVolume(int volume);
 
- signals:
-  void mutedChanged(bool value);
-  void volumeChanged(int value);
+signals:
+    void mutedChanged(bool value);
+    void volumeChanged(int value);
 
- private slots:
-  void setButtonIcon();
-  void setButtonSize();
+private slots:
+    void setButtonIcon();
+    void setButtonSize();
 
- private:
-  QSlider *slider;
-  QPushButton *muteButton;
+private:
+    QSlider *slider;
+    QPushButton *muteButton;
 
-  void createMuteButton();
-  void createSlider();
-  void createLayout();
-  void createConnections();
+    void createMuteButton();
+    void createSlider();
+    void createLayout();
+    void createConnections();
 };
