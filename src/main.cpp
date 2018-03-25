a44985eaef3994574771e4df5ad140050addf634
claus <claus.z@protonmail.com>
2018-03-25 20:14:28 +1100

    Update version number in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -45,7 +45,7 @@ int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
     // Setting the app-icon.
     app.setWindowIcon(QIcon(QPixmap(":/images/images/lala-icon-2-small.png")));
-    app.setApplicationVersion("3.4");
+    app.setApplicationVersion("3.5.0");
     app.setApplicationName("lalamachine");
     app.setOrganizationName("rmean");
 
