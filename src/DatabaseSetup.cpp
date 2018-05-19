1212068b79480b772cc4f89561b592d4aad2b49f
claus <claus.z@protonmail.com>
2018-05-19 15:37:56 +1000

    Remove maindb from DatabaseSetup

--- src/DatabaseSetup.cpp
+++ src/DatabaseSetup.cpp
@@ -6,13 +6,8 @@ DatabaseSetup::DatabaseSetup() {
     auto scannerDB = QSqlDatabase::addDatabase("QSQLITE", Config::SCANNERDB_NAME);
     scannerDB.setDatabaseName(Config::MUSICLIBDB);
     scannerDB.open();
-
-    auto mainDB = QSqlDatabase::addDatabase("QSQLITE", Config::MAINDB_NAME);
-    mainDB.setDatabaseName(Config::MUSICLIBDB);
-    mainDB.open();
 }
 
 DatabaseSetup::~DatabaseSetup() {
     QSqlDatabase::database(Config::SCANNERDB_NAME).close();
-    QSqlDatabase::database(Config::MAINDB_NAME).close();
 }
