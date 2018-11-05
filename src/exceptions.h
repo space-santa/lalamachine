4101882cbd935b0abfa2da90fe98671e44a6b653
claus <claus.z@protonmail.com>
2018-11-05 19:04:27 +1100

    Move definitions of short classes into the header

--- src/exceptions.h
+++ src/exceptions.h
@@ -3,25 +3,31 @@
 
 class AddQueryError : public std::runtime_error {
 public:
-    AddQueryError(const std::string& error);
+    AddQueryError(const std::string& error) : std::runtime_error(error) {
+    }
 };
 
 class OpenDatabaseError : public std::runtime_error {
 public:
-    OpenDatabaseError(const std::string& error);
+    OpenDatabaseError(const std::string& error) : std::runtime_error(error) {
+    }
 };
 
 class QueryError : public std::runtime_error {
 public:
-    QueryError(const std::string& error);
+    QueryError(const std::string& error) : std::runtime_error(error) {
+    }
 };
 
 class TableNotFoundError : public std::runtime_error {
 public:
-    TableNotFoundError(const std::string& tableName);
+    TableNotFoundError(const std::string& tableName)
+        : std::runtime_error(std::string("Table '") + tableName + std::string("' not found.")) {
+    }
 };
 
 class DatabaseCommitError : public std::runtime_error {
 public:
-    DatabaseCommitError(const std::string& text);
+    DatabaseCommitError(const std::string& text) : std::runtime_error(text) {
+    }
 };
