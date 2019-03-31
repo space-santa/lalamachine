0b539d44f4cd8f587dd8a7328fb0506513fa37d1
space-santa <claus.z@protonmail.com>
2019-03-31 20:13:28 +1100

    Add Entity Framework

---
+++ dotnet/Data/LalaContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dotnet.Data
+{
+    public class LalaContext : DbContext
+    {
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Track> Tracks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source={Constants.DB_PATH}");
+        }
+    }
+}
---
+++ dotnet/Data/Model.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dotnet
+{
+
+    public class Genre
+    {
+        public int GenreId { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Artist
+    {
+        public int ArtistId { get; set; }
+        public string Name { get; set; }
+
+    }
+
+    public class Album
+    {
+        public int AlbumId { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Track
+    {
+        public int TrackId { get; set; }
+        public string Title { get; set; }
+        public string Comment { get; set; }
+        public int TrackNumber { get; set; }
+        public string Path { get; set; }
+        public int Length { get; set; }
+        public int Year { get; set; }
+        public int DiscNumber { get; set; }
+
+        public int GenreId { get; set; }
+        public int ArtistId { get; set; }
+        public int AlbumId { get; set; }
+
+    }
+}
