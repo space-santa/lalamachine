9fa02b67dd3ae9dadbe7366b0588d404396a1806
space-santa <claus.z@protonmail.com>
2019-03-31 12:03:51 +1100

    Persist config

--- dotnet/Config.cs
+++ dotnet/Config.cs
@@ -1,5 +1,7 @@
-﻿using Qml.Net;
+﻿using Newtonsoft.Json;
+using Qml.Net;
 using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace dotnet
@@ -10,18 +12,57 @@ namespace dotnet
         public static string LALADIR = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "lalamachine");
         public static string SETTINGS_PATH = Path.Combine(LALADIR, "settings.json");
         public static string LIB_SETTINGS_PATH = Path.Combine(LALADIR, "libsettings.json");
+        public static string CONFIG_PATH = Path.Combine(LALADIR, "config.json");
 
-        public double volume { get; set; }
-        public string lastPlaylist { get; set; }
-        public string libPath { get; set; }
-        public string playlistColumns { get; set; }
+        public double volume
+        {
+            get => Convert.ToDouble(_Config["volume"]);
+            set => SetConfigValue("volume", value);
+        }
+
+        public string lastPlaylist
+        {
+            get => _Config["lastPlaylist"];
+            set => SetConfigValue("lastPlaylist", value);
+        }
+
+        public string libPath
+        {
+            get => _Config["libPath"];
+            set => SetConfigValue("libPath", value);
+        }
+
+        public string playlistColumns
+        {
+            get => _Config["playlistColumns"];
+            set => SetConfigValue("playlistColumns", value);
+        }
 
         public Config()
         {
-            volume = 0.5;
-            lastPlaylist = "";
-            libPath = "";
-            playlistColumns = @"
+            Init();
+
+            try
+            {
+                Load();
+            }
+            catch (DirectoryNotFoundException)
+            {
+                CreateLaladir();
+            }
+            catch (FileNotFoundException)
+            {
+                // Nothing to worry about, file just doesn't exist.
+            }
+        }
+
+        private void Init()
+        {
+            _Config = new Dictionary<string, string>();
+            _Config["volume"] = "0.5";
+            _Config["lastPlaylist"] = "";
+            _Config["libPath"] = "";
+            _Config["playlistColumns"] = @"
                 [
                     { ""key"": ""track"", ""value"": true },
                     { ""key"": ""discNumber"", ""value"": true },
@@ -35,9 +76,35 @@ namespace dotnet
                 ]";
         }
 
+        private void Load()
+        {
+            using (StreamReader file = File.OpenText(dotnet.Config.CONFIG_PATH))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                _Config = (Dictionary<string, string>)serializer.Deserialize(file, typeof(Dictionary<string, string>));
+            }
+        }
+
         public static void CreateLaladir()
         {
             Directory.CreateDirectory(LALADIR);
         }
+
+        private void SetConfigValue(string key, double value)
+        {
+            SetConfigValue(key, $"{value}");
+        }
+
+        private void SetConfigValue(string key, string value)
+        {
+            _Config[key] = value;
+            using (StreamWriter file = File.CreateText(dotnet.Config.CONFIG_PATH))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, _Config);
+            }
+        }
+
+        private Dictionary<string, string> _Config;
     }
 }
