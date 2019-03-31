8c52ba5e85e9005ec3e6ddf35d57f7b2f86151e6
space-santa <claus.z@protonmail.com>
2019-03-31 20:12:49 +1100

    Add MusicScanner

---
+++ MusicScanner.Unittests/MusicDatabaseTest.cs
@@ -0,0 +1,29 @@
+using System.IO;
+using Xunit;
+using TagReader;
+
+namespace MusicScanner.Unittests
+{
+    public class MusicDatabaseTest
+    {
+        [Fact]
+        public void TestShouldCreateDatabase()
+        {
+            var db = new MusicDatabase();
+            File.Delete(db.DbPath);
+            Assert.False(File.Exists(db.DbPath));
+            db.Connect("");
+            db.EnsureDatabase();
+            Assert.True(File.Exists(db.DbPath));
+            Tags tags = new Tags();
+            tags.album = "the album";
+            tags.title = "the title";
+            tags.length = 345;
+            tags.artist = new string[] {"the artist", "the other guy"};
+            tags.path = "/this/is/the/path";
+            db.AddTagsToDatabase(tags);
+            Tags result = db.GetTag(tags.path);
+            Assert.Equal(tags.artist[0], result.artist[0]);
+        }
+    }
+}
---
+++ MusicScanner/MusicScanner.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using TagReader;
+
+namespace MusicScanner
+{
+    public class MusicScanner
+    {
+        public static void ProcessDirectory(string path, IMusicDatabase db)
+        {
+            db.EnsureDatabase();
+
+            foreach (string file in DirSearch(path))
+            {
+                var tags = TagReader.TagReader.Read(file);
+                db.AddTagsToDatabase(tags);
+            }
+        }
+
+        private static IEnumerable<string> DirSearch(string basePath)
+        {
+            var files = Directory.EnumerateFiles(basePath, "*.*", SearchOption.AllDirectories)
+                .Where(s => s.EndsWith(".mp3") || s.EndsWith(".m4a"));
+            return files;
+        }
+    }
+}
