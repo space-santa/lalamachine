ff16f774cf43262980896ae8730ecbb0d9486479
claus <claus.z@protonmail.com>
2019-04-01 21:58:29 +1100

    Add initial migration

---
+++ dotnet/Migrations/20190401095917_InitialCreate.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dotnet.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Albums",
+                columns: table => new
+                {
+                    AlbumId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Albums", x => x.AlbumId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Artists",
+                columns: table => new
+                {
+                    ArtistId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Artists", x => x.ArtistId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Genres",
+                columns: table => new
+                {
+                    GenreId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genres", x => x.GenreId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tracks",
+                columns: table => new
+                {
+                    TrackId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(nullable: true),
+                    Comment = table.Column<string>(nullable: true),
+                    TrackNumber = table.Column<int>(nullable: false),
+                    Path = table.Column<string>(nullable: true),
+                    Length = table.Column<int>(nullable: false),
+                    Year = table.Column<int>(nullable: false),
+                    DiscNumber = table.Column<int>(nullable: false),
+                    GenreId = table.Column<int>(nullable: false),
+                    ArtistId = table.Column<int>(nullable: false),
+                    AlbumId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tracks", x => x.TrackId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Albums");
+
+            migrationBuilder.DropTable(
+                name: "Artists");
+
+            migrationBuilder.DropTable(
+                name: "Genres");
+
+            migrationBuilder.DropTable(
+                name: "Tracks");
+        }
+    }
+}
--- dotnet/MusicLib.cs
+++ dotnet/MusicLib.cs
@@ -1,7 +1,18 @@
-﻿namespace dotnet
+﻿using dotnet.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet
 {
     public class MusicLib
     {
+        LalaContext _context;
+
+        public MusicLib()
+        {
+            _context = new LalaContext();
+            _context.Database.Migrate();
+        }
+
         public bool scanning { get; set; }
 
         public void getMetadataForMrl(string mrl)
