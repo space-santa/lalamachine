eaecbd7ed5ec5bd011ceb63403e34c42f27518a6
claus <claus.z@protonmail.com>
2019-04-01 21:58:54 +1100

    Add skel implementation of IMusicDatabase

---
+++ dotnet/ScannerDb.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using dotnet.Data;
+using MusicScanner;
+using TagReader;
+
+namespace dotnet
+{
+    public class ScannerDb : IMusicDatabase
+    {
+        LalaContext _context;
+
+        public ScannerDb(LalaContext Context)
+        {
+            _context = Context;
+        }
+
+        public void AddTagsToDatabase(Tags tags)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EnsureDatabase()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
