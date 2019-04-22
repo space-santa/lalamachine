9e3bf6eaa52f8c0ceb29eef54701b6b2a2bc3f98
space-santa <claus.z@protonmail.com>
2019-04-22 10:55:12 +1000

    Move View stuff into View folder

--- Lalamachine.Wpf/View/MainWindow.xaml.cs
+++ Lalamachine.Wpf/View/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Lalamachine.Wpf
+namespace Lalamachine.Wpf.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
