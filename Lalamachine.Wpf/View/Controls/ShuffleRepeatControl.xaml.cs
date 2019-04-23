522d0de89b9e61a811232c5355545a1d365fd796
space-santa <claus.z@protonmail.com>
2019-04-23 17:04:10 +1000

    Add ShuffleRepeatControl

---
+++ Lalamachine.Wpf/View/Controls/ShuffleRepeatControl.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lalamachine.Wpf.View.Controls
+{
+    public enum ShuffleRepeatState { None, RepeatOne, RepeatAll, Shuffle };
+
+    public partial class ShuffleRepeatControl : UserControl, INotifyPropertyChanged
+    {
+        private ShuffleRepeatState state;
+        public ShuffleRepeatState State
+        {
+            get => state;
+            set
+            {
+                state = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public ShuffleRepeatControl()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            switch (State)
+            {
+                case ShuffleRepeatState.RepeatOne:
+                    State = ShuffleRepeatState.RepeatAll;
+                    break;
+                case ShuffleRepeatState.RepeatAll:
+                    State = ShuffleRepeatState.Shuffle;
+                    break;
+                case ShuffleRepeatState.None:
+                    State = ShuffleRepeatState.RepeatOne;
+                    break;
+                default:
+                    State = ShuffleRepeatState.None;
+                    break;
+            }
+        }
+    }
+}
---
+++ Lalamachine.Wpf/View/Converter/ShuffleRepeatContentConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Data;
+using Lalamachine.Wpf.View.Controls;
+
+namespace Lalamachine.Wpf.View.Converter
+{
+    [ValueConversion(typeof(ShuffleRepeatState), typeof(string))]
+    class ShuffleRepeatContentConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var state = (ShuffleRepeatState)value;
+            
+            switch (state)
+            {
+                case ShuffleRepeatState.RepeatOne:
+                    return "🔂";
+                case ShuffleRepeatState.RepeatAll:
+                    return "🔁";
+                case ShuffleRepeatState.Shuffle:
+                    return "🔀";
+                default:
+                    return "🎵";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
