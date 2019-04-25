da6d206c9d3c5ea84a7e20e321e311c4af6fc6cb
space-santa <claus.z@protonmail.com>
2019-04-25 19:32:01 +1000

    Add PlayerButtonsControl

---
+++ Lalamachine.Wpf/View/Controls/PlayerButtonsControl.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lalamachine.Wpf.View.Controls
+{
+    /// <summary>
+    /// Interaction logic for PlayerButtonsControl.xaml
+    /// </summary>
+    public partial class PlayerButtonsControl : UserControl
+    {
+        public PlayerButtonsControl()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PlayPauseButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ForwardButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
