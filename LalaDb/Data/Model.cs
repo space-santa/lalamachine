0c5034377f4c12163808c01e8458e755f8187644
claus <claus.z@protonmail.com>
2019-05-08 16:56:16 +1000

    Add LalaDb

---
+++ LalaDb/Data/Model.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LalaDb.Data
+{
+
+    public class Genre
+    {
+        public int GenreId { get; set; }
+        public string Name { get; set; }
+
+        public virtual List<GenreTrack> GenreTracks { get; set; }
+    }
+
+    public class Artist
+    {
+        public int ArtistId { get; set; }
+        public string Name { get; set; }
+
+        public virtual List<ArtistTrack> ArtistTracks { get; set; }
+    }
+
+    public class Album
+    {
+        public int AlbumId { get; set; }
+        public string Name { get; set; }
+
+        public virtual List<Track> Tracks { get; set; }
+    }
+
+    public class Track
+    {
+        public int TrackId { get; set; }
+        public string Title { get; set; }
+        public string Comment { get; set; }
+        public int TrackNumber { get; set; }
+        public string Path { get; set; }
+        public int Length { get; set; }
+        public int Year { get; set; }
+        public int DiscNumber { get; set; }
+
+        public int AlbumId { get; set; }
+        public virtual Album Album { get; set; }
+
+        public virtual List<ArtistTrack> ArtistTracks { get; set; }
+        public virtual List<GenreTrack> GenreTracks { get; set; }
+    }
+
+
+    public class ArtistTrack
+    {
+        public int ArtistTrackId { get; set; }
+
+        public int ArtistId { get; set; }
+        public virtual Artist Artist { get; set; }
+
+        public int TrackId { get; set; }
+        public virtual Track Track { get; set; }
+    }
+
+
+    public class GenreTrack
+    {
+        public int GenreTrackId { get; set; }
+
+        public int GenreId { get; set; }
+        public virtual Genre Genre { get; set; }
+
+        public int TrackId { get; set; }
+        public virtual Track Track { get; set; }
+    }
+}
---
+++ LalaDb/Data/ScannerDb.cs
@@ -0,0 +1,100 @@
+﻿using System.Linq;
+using LibLala.MusicScanner;
+
+namespace LalaDb.Data
+{
+    public class ScannerDb : IMusicDatabase
+    {
+        LalaContext _context;
+
+        public ScannerDb(LalaContext Context)
+        {
+            _context = Context;
+        }
+
+        public void AddTagsToDatabase(LibLala.TagReader.Tags tags)
+        {
+            Track track = new Track();
+
+            foreach (var g in tags.genre)
+            {
+                Genre genre;
+
+                if (_context.Genres.Any(x => x.Name == g))
+                {
+                    genre = _context.Genres.Single(x => x.Name == g);
+                }
+                else
+                {
+                    genre = new Genre
+                    {
+                        Name = g
+                    };
+
+                    _context.Genres.Add(genre);
+                }
+
+                _context.GenreTracks.Add(new GenreTrack { Genre = genre, Track = track });
+            }
+
+            foreach (var a in tags.Artist)
+            {
+                Artist artist;
+
+                if (_context.Artists.Any(x => x.Name == a))
+                {
+                    artist = _context.Artists.Single(x => x.Name == a);
+                }
+                else
+                {
+                    artist = new Artist
+                    {
+                        Name = a
+                    };
+                    _context.Artists.Add(artist);
+                }
+
+
+                _context.ArtistTracks.Add(new ArtistTrack { Artist = artist, Track = track });
+            }
+
+            if (_context.Albums.Any(x => x.Name == tags.album))
+            {
+                track.Album = _context.Albums.Single(x => x.Name == tags.album);
+            }
+            else
+            {
+                track.Album = new Album
+                {
+                    Name = tags.album
+                };
+            }
+
+            track.Comment = tags.comment;
+            track.DiscNumber = (int)tags.discNumber;
+            track.Length = tags.length;
+            track.Path = tags.path;
+            track.Title = tags.title;
+            track.TrackNumber = (int)tags.track;
+            track.Year = (int)tags.year;
+
+            _context.Tracks.Add(track);
+            _context.SaveChanges();
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        public void EnsureDatabase()
+        {
+            _context.Albums.RemoveRange(_context.Albums);
+            _context.Artists.RemoveRange(_context.Artists);
+            _context.ArtistTracks.RemoveRange(_context.ArtistTracks);
+            _context.Genres.RemoveRange(_context.Genres);
+            _context.GenreTracks.RemoveRange(_context.GenreTracks);
+            _context.Tracks.RemoveRange(_context.Tracks);
+        }
+    }
+}
