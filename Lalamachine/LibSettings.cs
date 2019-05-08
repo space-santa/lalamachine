2cf75932ff7e9fc38d959f95e0d3ad91aba0bf46
claus <claus.z@protonmail.com>
2019-05-08 17:25:58 +1000

    Remove unnecessary usings

--- Lalamachine/LibSettings.cs
+++ Lalamachine/LibSettings.cs
@@ -1,10 +1,4 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
-
-namespace Lalamachine
+﻿namespace Lalamachine
 {
     public class LibSettings : AbstractBaseSettings
     {
