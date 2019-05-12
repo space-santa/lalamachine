d63517ed11b9cab97c38fb7d9d411fabef106975
space-santa <claus.z@protonmail.com>
2019-05-12 19:51:19 +1000

    Display the position and duration as m:ss strings

---
+++ Lalamachine.Wpf/View/Converter/SecondsToTimeSpanConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace Lalamachine.Wpf.View.Converter
+{
+    [ValueConversion(typeof(double), typeof(string))]
+    public class SecondsToTimeSpanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            double seconds = (double)value;
+            TimeSpan timeSpan = new TimeSpan(0, 0, (int)seconds);
+            return timeSpan.ToString(@"m\:ss");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
