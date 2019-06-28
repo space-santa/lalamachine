18e323540ab5fad7e009d0057114d28922a457cd
claus <claus.z@protonmail.com>
2019-06-28 21:50:46 +1000

    Add LibraryControl skel

---
+++ Lalamachine.Wpf/View/Controls/LibraryControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lalamachine.Wpf.View.Controls
+{
+    /// <summary>
+    /// Interaction logic for LibraryControl.xaml
+    /// </summary>
+    public partial class LibraryControl : UserControl
+    {
+        public LibraryControl()
+        {
+            InitializeComponent();
+        }
+
+        private void GenreListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
