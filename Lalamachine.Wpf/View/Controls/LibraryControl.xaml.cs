360727d428ca9b20297c96678b9d7108b4f2b8fa
claus <claus.z@protonmail.com>
2019-08-04 13:11:13 +1000

    Update list on search text change
    Because the databinding only triggers when the component loses focus

--- Lalamachine.Wpf/View/Controls/LibraryControl.xaml.cs
+++ Lalamachine.Wpf/View/Controls/LibraryControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using Lalamachine.Wpf.ViewModel;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace Lalamachine.Wpf.View.Controls
@@ -21,6 +22,13 @@ namespace Lalamachine.Wpf.View.Controls
         private void ListViewItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
 
+        }
+
+        private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (SearchBox.Text.Length < 3) { return; }
+            LibraryViewModel context = (LibraryViewModel)DataContext;
+            context.UpdateListsCommand.Execute(null);
         }
     }
 }
