730f790bbaa0620a223bce2da4002787429c0e23
claus <claus.z@protonmail.com>
2019-08-15 21:12:02 +1000

    Test RemoveFilePrefix

---
+++ LibLala.Unittests/UtilsTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace LibLala.Unittests
+{
+    internal class UtilsTest
+    {
+        [Test]
+        public void RemoveFilePrefix_NoPrefix_UnchangedString()
+        {
+            var result = Utils.RemoveFilePrefix("bob goes to town");
+            var expected = "bob goes to town";
+            Assert.AreEqual(result, expected);
+        }
+
+        [Test]
+        public void RemoveFilePrefix_FilePrefix_CeanString()
+        {
+            var result = Utils.RemoveFilePrefix("file:////d/e/f");
+            var expected = "/d/e/f";
+            Assert.AreEqual(result, expected);
+        }
+    }
+}
