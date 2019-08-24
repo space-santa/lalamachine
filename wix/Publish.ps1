6d9303531b517318f213e6809e14c6f29f8e476b
claus <claus.z@protonmail.com>
2019-08-24 16:03:23 +1000

    v4.99.6

--- wix/Publish.ps1
+++ wix/Publish.ps1
@@ -1,4 +1,4 @@
-$version = "4.99.5"
+$version = "4.99.6"
 $PathToPublish = "..\Lalamachine.Wpf\bin\Release\netcoreapp3.0\win-x64\publish"
 
 dotnet publish ../Lalamachine.Wpf/Lalamachine.Wpf.csproj -r win-x64 -c Release -p:PublishSingleFile=true
