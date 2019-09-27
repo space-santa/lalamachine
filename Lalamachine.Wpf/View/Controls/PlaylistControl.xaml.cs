32e7857fbe4e441003a2cf66a28cac69b53b3139
space-santa <claus.z@protonmail.com>
2019-09-27 20:11:38 +1000

    Sort the list
    This does not sort the actual list, just the view.

--- Lalamachine.Wpf/View/Controls/PlaylistControl.xaml.cs
+++ Lalamachine.Wpf/View/Controls/PlaylistControl.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Input;
 using Lalamachine.Wpf.ViewModel;
 
@@ -9,6 +12,9 @@ namespace Lalamachine.Wpf.View.Controls
     /// </summary>
     public partial class PlaylistControl : UserControl
     {
+        GridViewColumnHeader? _lastHeaderClicked = null;
+        ListSortDirection _lastDirection = ListSortDirection.Ascending;
+
         public PlaylistControl()
         {
             InitializeComponent();
@@ -18,6 +24,69 @@ namespace Lalamachine.Wpf.View.Controls
         {
             var viewModel = (PlaylistViewModel)DataContext;
             viewModel.PlayTrackCommand.Execute(PlaylistView.SelectedItem);
+        }
+
+        // TODO: Add sort of the actual list to the ViewModel as a command. This does only sort the view which is useless.
+        private void PlaylistViewColumnHeader_Click(object sender, RoutedEventArgs e)
+        {
+            var headerClicked = e.OriginalSource as GridViewColumnHeader;
+            ListSortDirection direction;
+
+            if (headerClicked != null)
+            {
+                if (headerClicked.Role != GridViewColumnHeaderRole.Padding)
+                {
+                    if (headerClicked != _lastHeaderClicked)
+                    {
+                        direction = ListSortDirection.Ascending;
+                    }
+                    else
+                    {
+                        if (_lastDirection == ListSortDirection.Ascending)
+                        {
+                            direction = ListSortDirection.Descending;
+                        }
+                        else
+                        {
+                            direction = ListSortDirection.Ascending;
+                        }
+                    }
+
+                    var columnBinding = headerClicked.Column.DisplayMemberBinding as Binding;
+                    var sortBy = columnBinding?.Path.Path ?? headerClicked.Column.Header as string;
+
+                    Sort(sortBy, direction);
+
+                    if (direction == ListSortDirection.Ascending)
+                    {
+                        headerClicked.Column.SetCurrentValue(GridViewColumn.HeaderTemplateProperty, Resources["HeaderTemplateArrowUp"] as DataTemplate);
+                    }
+                    else
+                    {
+                        headerClicked.Column.SetCurrentValue(GridViewColumn.HeaderTemplateProperty, Resources["HeaderTemplateArrowDown"] as DataTemplate);
+                    }
+
+                    // Remove arrow from previously sorted header
+                    if (_lastHeaderClicked != null && _lastHeaderClicked != headerClicked)
+                    {
+                        _lastHeaderClicked.Column.SetCurrentValue(GridViewColumn.HeaderTemplateProperty, null);
+                    }
+
+                    _lastHeaderClicked = headerClicked;
+                    _lastDirection = direction;
+                }
+            }
+        }
+
+        private void Sort(string? sortBy, ListSortDirection direction)
+        {
+            if (sortBy is null) return;
+
+            var dataView = CollectionViewSource.GetDefaultView(PlaylistView.ItemsSource);
+            dataView.SortDescriptions.Clear();
+            var sd = new SortDescription(sortBy, direction);
+            dataView.SortDescriptions.Add(sd);
+            dataView.Refresh();
         }
     }
 }
