94e24a1d92fe41dd5bfe6ffc57e9f960a0d617c8
claus <claus.z@protonmail.com>
2019-09-28 18:13:21 +1000

    Culture to convert timestring

--- Lalamachine.Wpf/View/Converter/SecondsToTimeSpanConverter.cs
+++ Lalamachine.Wpf/View/Converter/SecondsToTimeSpanConverter.cs
@@ -6,22 +6,21 @@ namespace Lalamachine.Wpf.View.Converter
     [ValueConversion(typeof(double), typeof(string))]
     public class SecondsToTimeSpanConverter : IValueConverter
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1305:Specify IFormatProvider", Justification = "<Pending>")]
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             var seconds = value is double ? (double)value : (int)value;
             var timeSpan = new TimeSpan(0, 0, (int)seconds);
             if (timeSpan.TotalDays >= 1)
             {
-                return timeSpan.ToString(@"d' days, 'h\:mm\:ss");
+                return timeSpan.ToString(@"d' days, 'h\:mm\:ss", LibLala.Constants.CULTURE);
             }
             else if (timeSpan.TotalMinutes > 59)
             {
-                return timeSpan.ToString(@"h\:mm\:ss");
+                return timeSpan.ToString(@"h\:mm\:ss", LibLala.Constants.CULTURE);
             }
             else
             {
-                return timeSpan.ToString(@"m\:ss");
+                return timeSpan.ToString(@"m\:ss", LibLala.Constants.CULTURE);
             }
         }
 
