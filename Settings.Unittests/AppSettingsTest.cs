70816c1beeca757f80006ef847c82f89f8f00e1d
space-santa <claus.z@protonmail.com>
2019-10-02 20:16:49 +1000

    Fix some naming violations

--- Settings.Unittests/AppSettingsTest.cs
+++ Settings.Unittests/AppSettingsTest.cs
@@ -20,49 +20,49 @@ namespace Settings.Unittests
         [Test]
         public void TestIntShouldSave()
         {
-            var SettingsFile = Substitute.For<SettingsFile>("Joe", "Bob");
-            SettingsFile.Load().Returns(new Dictionary<string, string>());
-            var settings = new TestSettings(SettingsFile)
+            var settingsFile = Substitute.For<SettingsFile>("Joe", "Bob");
+            settingsFile.Load().Returns(new Dictionary<string, string>());
+            var settings = new TestSettings(settingsFile)
             {
                 TestInt = 5
             };
-            SettingsFile.Received().Save(Arg.Is<Dictionary<string, string>>(x => x["TestInt"] == "5"));
+            settingsFile.Received().Save(Arg.Is<Dictionary<string, string>>(x => x["TestInt"] == "5"));
         }
 
         [Test]
         public void TestBoolShouldSave()
         {
-            var SettingsFile = Substitute.For<SettingsFile>("Joe", "Bob");
-            SettingsFile.Load().Returns(new Dictionary<string, string>());
-            var settings = new TestSettings(SettingsFile)
+            var settingsFile = Substitute.For<SettingsFile>("Joe", "Bob");
+            settingsFile.Load().Returns(new Dictionary<string, string>());
+            var settings = new TestSettings(settingsFile)
             {
                 TestBool = true
             };
-            SettingsFile.Received().Save(Arg.Is<Dictionary<string, string>>(x => x["TestBool"] == true.ToString(LibLala.Constants.CULTURE)));
+            settingsFile.Received().Save(Arg.Is<Dictionary<string, string>>(x => x["TestBool"] == true.ToString(LibLala.Constants.CULTURE)));
         }
 
         [Test]
         public void TestDoubleShouldSave()
         {
-            var SettingsFile = Substitute.For<SettingsFile>("Joe", "Bob");
-            SettingsFile.Load().Returns(new Dictionary<string, string>());
-            var settings = new TestSettings(SettingsFile)
+            var settingsFile = Substitute.For<SettingsFile>("Joe", "Bob");
+            settingsFile.Load().Returns(new Dictionary<string, string>());
+            var settings = new TestSettings(settingsFile)
             {
                 TestDouble = 5.5
             };
-            SettingsFile.Received().Save(Arg.Is<Dictionary<string, string>>(x => x["TestDouble"] == "5.5"));
+            settingsFile.Received().Save(Arg.Is<Dictionary<string, string>>(x => x["TestDouble"] == "5.5"));
         }
 
         [Test]
         public void TestStringShouldSave()
         {
-            var SettingsFile = Substitute.For<SettingsFile>("Joe", "Bob");
-            SettingsFile.Load().Returns(new Dictionary<string, string>());
-            var settings = new TestSettings(SettingsFile)
+            var settingsFile = Substitute.For<SettingsFile>("Joe", "Bob");
+            settingsFile.Load().Returns(new Dictionary<string, string>());
+            var settings = new TestSettings(settingsFile)
             {
                 TestString = "Bob goes to town."
             };
-            SettingsFile.Received().Save(Arg.Is<Dictionary<string, string>>(x => x["TestString"] == "Bob goes to town."));
+            settingsFile.Received().Save(Arg.Is<Dictionary<string, string>>(x => x["TestString"] == "Bob goes to town."));
         }
     }
 }
