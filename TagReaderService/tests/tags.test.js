8527dc2e1e9f8a619057c9a212cb56920cb6f4a3
space-santa <claus.z@protonmail.com>
2019-12-26 22:48:28 +1100

    Test different kinds of genre tags

--- TagReaderService/tests/tags.test.js
+++ TagReaderService/tests/tags.test.js
@@ -1,10 +1,20 @@
 const request = require("supertest");
 const app = require("../src/app");
 
-test("Should get tags object when posting a file to /tags", async () => {
+test("Should get correct genre string from number when posting a file to /tags", async () => {
   const response = await request(app)
     .post("/tags")
     .attach("file", "tests/fixtures/good.mp3")
     .expect(200);
   console.log(response.body);
+  expect(response.body.genre).toEqual("Alternative");
+});
+
+test("Should get correct genre string object when posting a file to /tags", async () => {
+  const response = await request(app)
+    .post("/tags")
+    .attach("file", "tests/fixtures/genre.mp3")
+    .expect(200);
+  console.log(response.body);
+  expect(response.body.genre).toEqual("Garage / Bassline / Grime");
 });
