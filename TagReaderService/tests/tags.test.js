d84797be469cefa7831bacc86fe163aea8a954d1
space-santa <claus.z@protonmail.com>
2019-12-27 17:35:08 +1100

    Test extract directly

--- TagReaderService/tests/tags.test.js
+++ TagReaderService/tests/tags.test.js
@@ -24,3 +24,42 @@ test("Should get the discnumber if it has one", async () => {
     .expect(200);
   expect(response.body.disc).toEqual("2/2");
 });
+
+const extract = require("../src/tags");
+
+const testTags = {
+  title: "bob title",
+  artist: "bob artist",
+  album: "bob album",
+  comment: {
+    text: "bob comment"
+  },
+  track: "1/19",
+  genre: "20",
+  TPOS: {
+    data: "2/9"
+  },
+  year: "2005",
+  TBPM: {
+    data: "123"
+  },
+  TKEY: {
+    data: "7A"
+  }
+};
+
+test("Should get tags from testTags", async () => {
+  const tags = extract(testTags);
+  expect(tags).toEqual({
+    title: "bob title",
+    artist: "bob artist",
+    album: "bob album",
+    comment: "bob comment",
+    track: "1/19",
+    genre: "Alternative",
+    disc: "2/9",
+    year: "2005",
+    BPM: "123",
+    KEY: "7A"
+  });
+});
