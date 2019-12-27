15358a3f826613d51dc851bd279e7fbbcc809b9b
space-santa <claus.z@protonmail.com>
2019-12-27 18:01:08 +1100

    Don't include the total number of tracks or discs
    E.g. only use the number before the /

--- TagReaderService/src/tags.js
+++ TagReaderService/src/tags.js
@@ -1,17 +1,24 @@
 const genreLookup = require("id3-genre");
 
+const getTrackOrDiscWithoutSlash = value => {
+  if (value.indexOf("/") > 0) {
+    return value.split("/")[0];
+  }
+  return value;
+};
+
 const extract = json => {
   const tags = {
     title: json.title,
     artist: json.artist,
     album: json.album,
     comment: json.comment.text ? json.comment.text : json.comment,
-    track: json.track,
+    track: getTrackOrDiscWithoutSlash(json.track),
     genre:
       genreLookup(+json.genre) === "Unknown"
         ? json.genre
         : genreLookup(+json.genre),
-    disc: json.TPOS ? json.TPOS.data : undefined,
+    disc: json.TPOS ? getTrackOrDiscWithoutSlash(json.TPOS.data) : undefined,
     year: json.year ? json.year : json.TDRC ? json.TDRC.data : undefined,
     BPM: json.TBPM ? json.TBPM.data : undefined,
     KEY: json.TKEY ? json.TKEY.data : undefined
