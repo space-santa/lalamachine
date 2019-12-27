610daf6fdacf5ddb78039c4241a73b08056d148a
space-santa <claus.z@protonmail.com>
2019-12-27 13:29:28 +1100

    Update test to use proper http verb

--- TagReaderService/tests/tags.test.js
+++ TagReaderService/tests/tags.test.js
@@ -3,7 +3,7 @@ const app = require("../src/app");
 
 test("Should get correct genre string from number when posting a file to /tags", async () => {
   const response = await request(app)
-    .post("/tags")
+    .get("/tags")
     .attach("file", "tests/fixtures/good.mp3")
     .expect(200);
   console.log(response.body);
@@ -12,7 +12,7 @@ test("Should get correct genre string from number when posting a file to /tags",
 
 test("Should get correct genre string object when posting a file to /tags", async () => {
   const response = await request(app)
-    .post("/tags")
+    .get("/tags")
     .attach("file", "tests/fixtures/genre.mp3")
     .expect(200);
   console.log(response.body);
