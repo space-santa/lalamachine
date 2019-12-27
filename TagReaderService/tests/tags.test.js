64b206343a7dff8b120f187418385a184b042f0f
space-santa <claus.z@protonmail.com>
2019-12-27 13:41:09 +1100

    Add test for discnumber

--- TagReaderService/tests/tags.test.js
+++ TagReaderService/tests/tags.test.js
@@ -4,17 +4,23 @@ const app = require("../src/app");
 test("Should get correct genre string from number when posting a file to /tags", async () => {
   const response = await request(app)
     .get("/tags")
-    .attach("file", "tests/fixtures/good.mp3")
+    .attach("music", "tests/fixtures/good.mp3")
     .expect(200);
-  console.log(response.body);
   expect(response.body.genre).toEqual("Alternative");
 });
 
 test("Should get correct genre string object when posting a file to /tags", async () => {
   const response = await request(app)
     .get("/tags")
-    .attach("file", "tests/fixtures/genre.mp3")
+    .attach("music", "tests/fixtures/genre.mp3")
     .expect(200);
-  console.log(response.body);
   expect(response.body.genre).toEqual("Garage / Bassline / Grime");
+});
+
+test("Should get the discnumber if it has one", async () => {
+  const response = await request(app)
+    .get("/tags")
+    .attach("music", "tests/fixtures/disc.mp3")
+    .expect(200);
+  expect(response.body.disc).toEqual("2/2");
 });
