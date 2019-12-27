6489ce3d894cefcb77259291ece37228c988777c
space-santa <claus.z@protonmail.com>
2019-12-27 18:01:17 +1100

    Increase coverage

--- TagReaderService/tests/tags.test.js
+++ TagReaderService/tests/tags.test.js
@@ -22,7 +22,7 @@ test("Should get the discnumber if it has one", async () => {
     .get("/tags")
     .attach("music", "tests/fixtures/disc.mp3")
     .expect(200);
-  expect(response.body.disc).toEqual("2/2");
+  expect(response.body.disc).toEqual("2");
 });
 
 const extract = require("../src/tags");
@@ -31,35 +31,67 @@ const testTags = {
   title: "bob title",
   artist: "bob artist",
   album: "bob album",
-  comment: {
-    text: "bob comment"
-  },
+  comment: { text: "bob comment" },
   track: "1/19",
   genre: "20",
-  TPOS: {
-    data: "2/9"
-  },
-  year: "2005",
-  TBPM: {
-    data: "123"
-  },
-  TKEY: {
-    data: "7A"
-  }
+  TPOS: { data: "2/9" },
+  TDRC: { data: "2005" },
+  TBPM: { data: "123" },
+  TKEY: { data: "7A" }
+};
+
+const getTestTags = () => {
+  return JSON.parse(JSON.stringify(testTags));
 };
 
 test("Should get tags from testTags", async () => {
-  const tags = extract(testTags);
+  const tags = extract(getTestTags());
   expect(tags).toEqual({
     title: "bob title",
     artist: "bob artist",
     album: "bob album",
     comment: "bob comment",
-    track: "1/19",
+    track: "1",
     genre: "Alternative",
-    disc: "2/9",
+    disc: "2",
     year: "2005",
     BPM: "123",
     KEY: "7A"
   });
+});
+
+test("Should get string comment", async () => {
+  let localTestTags = getTestTags();
+  localTestTags.comment = "new bob comment";
+  const tags = extract(localTestTags);
+  expect(tags.comment).toEqual("new bob comment");
+});
+
+test("Should get year from year id3 tag", async () => {
+  let localTestTags = getTestTags();
+  delete localTestTags.TDRC;
+  localTestTags.year = "1997";
+  const tags = extract(localTestTags);
+  expect(tags.year).toEqual("1997");
+});
+
+test("Should not include year in tags without year or TDRC id3 tag", async () => {
+  let localTestTags = getTestTags();
+  delete localTestTags.TDRC;
+  const tags = extract(localTestTags);
+  expect(tags.year).toBeUndefined();
+});
+
+test("Should not include BPM in tags without TBPM id3 tag", async () => {
+  let localTestTags = getTestTags();
+  delete localTestTags.TBPM;
+  const tags = extract(localTestTags);
+  expect(tags.BPM).toBeUndefined();
+});
+
+test("Should not include KEY in tags without TKEY id3 tag", async () => {
+  let localTestTags = getTestTags();
+  delete localTestTags.TKEY;
+  const tags = extract(localTestTags);
+  expect(tags.KEY).toBeUndefined();
 });
