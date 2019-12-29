94b168453c9deb12e737cbcc121b0e4287e8480e
claus <claus.z@protonmail.com>
2019-12-30 10:02:34 +1100

    Album name mustn't be unique

--- TagReaderService/src/models/album.js
+++ TagReaderService/src/models/album.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const albumSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true, trim: true, unique: true },
+    name: { type: String, required: true, trim: true },
     artist: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
