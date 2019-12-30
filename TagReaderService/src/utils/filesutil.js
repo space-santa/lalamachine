2ea7d35d2bc0e0b0b6767b4109b38611b063a3d2
claus <claus.z@protonmail.com>
2019-12-30 12:53:45 +1100

    Refactor saveFile
    Move save genre, artist, and album into dedicated functions.

--- TagReaderService/src/utils/filesutil.js
+++ TagReaderService/src/utils/filesutil.js
@@ -15,6 +15,61 @@ const getDirname = tags => {
   return `${process.env.MUSIC_DIR}/${genre}/${artist}/${album}`;
 };
 
+const saveGenre = async jsonTags => {
+  if (jsonTags.genre) {
+    genre = await Genre.findOne({ name: jsonTags.genre });
+    if (genre) {
+      jsonTags.genre = genre;
+    } else {
+      jsonTags.genre = new Genre({ name: jsonTags.genre });
+      await jsonTags.genre.save();
+    }
+  }
+};
+
+const saveArtist = async jsonTags => {
+  if (jsonTags.artist) {
+    artist = await Artist.findOne({ name: jsonTags.artist });
+    if (artist) {
+      jsonTags.artist = artist;
+    } else {
+      jsonTags.artist = new Artist({ name: jsonTags.artist });
+    }
+
+    if (jsonTags.genre) {
+      jsonTags.artist.genre = jsonTags.genre;
+    }
+
+    await jsonTags.artist.save();
+  }
+};
+
+const saveAlbum = async jsonTags => {
+  if (jsonTags.album) {
+    if (!jsonTags.artist) {
+      throw new Error("Album must have an artist.");
+    }
+
+    const album = await Album.findOne({
+      name: jsonTags.album,
+      artist: jsonTags.artist._id
+    });
+    if (album) {
+      jsonTags.album = album;
+    } else {
+      jsonTags.album = new Album({ name: jsonTags.album });
+
+      if (jsonTags.genre) {
+        jsonTags.album.genre = jsonTags.genre;
+      }
+
+      jsonTags.album.artist = jsonTags.artist;
+
+      await jsonTags.album.save();
+    }
+  }
+};
+
 const saveFile = file => {
   return new Promise(async resolve => {
     const jsonTags = await TagsUtil.getTags(file);
@@ -28,53 +83,12 @@ const saveFile = file => {
     }
     fs.writeFileSync(fileUri, file.buffer);
 
-    if (jsonTags.genre) {
-      genre = await Genre.findOne({ name: jsonTags.genre });
-      if (genre) {
-        jsonTags.genre = genre;
-      } else {
-        jsonTags.genre = new Genre({ name: jsonTags.genre });
-        await jsonTags.genre.save();
-      }
-    }
-
-    if (jsonTags.artist) {
-      artist = await Artist.findOne({ name: jsonTags.artist });
-      if (artist) {
-        jsonTags.artist = artist;
-      } else {
-        jsonTags.artist = new Artist({ name: jsonTags.artist });
-      }
-
-      if (jsonTags.genre) {
-        jsonTags.artist.genre = jsonTags.genre;
-      }
-
-      await jsonTags.artist.save();
-    }
-
-    if (jsonTags.album) {
-      if (!jsonTags.artist) {
-        return reject("Album must have an artist.");
-      }
-
-      const album = await Album.findOne({
-        name: jsonTags.album,
-        artist: jsonTags.artist._id
-      });
-      if (album) {
-        jsonTags.album = album;
-      } else {
-        jsonTags.album = new Album({ name: jsonTags.album });
-
-        if (jsonTags.genre) {
-          jsonTags.album.genre = jsonTags.genre;
-        }
-
-        jsonTags.album.artist = jsonTags.artist;
-
-        await jsonTags.album.save();
-      }
+    try {
+      await saveGenre(jsonTags);
+      await saveArtist(jsonTags);
+      await saveAlbum(jsonTags);
+    } catch (error) {
+      return reject(error);
     }
 
     const tags = new Tags(jsonTags);
