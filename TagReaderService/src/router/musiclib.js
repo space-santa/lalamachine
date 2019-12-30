839fe93f6bb5db150f1fe4a7a4f957d88ea15fbf
claus <claus.z@protonmail.com>
2019-12-30 17:18:32 +1100

    Update musiclib endpoints to use the relevant models

--- TagReaderService/src/router/musiclib.js
+++ TagReaderService/src/router/musiclib.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = new express.Router();
 
 const Tags = require("../models/tags");
+const Genre = require("../models/genre");
+const Artist = require("../models/artist");
+const Album = require("../models/album");
 
 router.get("/titles", async (req, res) => {
   try {
@@ -14,8 +17,26 @@ router.get("/titles", async (req, res) => {
 
 router.get("/genres", async (req, res) => {
   try {
-    const genres = await Tags.find({}, "genre");
+    const genres = await Genre.find({});
     return res.send(genres);
+  } catch (error) {
+    return res.status(500).send({ error });
+  }
+});
+
+router.get("/artists", async (req, res) => {
+  try {
+    const artists = await Artist.find({});
+    return res.send(artists);
+  } catch (error) {
+    return res.status(500).send({ error });
+  }
+});
+
+router.get("/albums", async (req, res) => {
+  try {
+    const albums = await Album.find({});
+    return res.send(albums);
   } catch (error) {
     return res.status(500).send({ error });
   }
