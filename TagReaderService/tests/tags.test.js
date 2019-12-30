62b23dc143081646f551ace38551cc793a283f9d
space-santa <claus.z@protonmail.com>
2019-12-31 08:41:25 +1100

    Refactor tags.test

--- TagReaderService/tests/tags.test.js
+++ TagReaderService/tests/tags.test.js
@@ -1,5 +1,7 @@
 const request = require("supertest");
 const app = require("../src/app");
+const getTestTags = require("./fixtures/tagsfactory");
+const TagsUtil = require("../src/utils/tagsutil");
 
 test("Should get correct genre string from number when posting a file to /tags", async () => {
   const response = await request(app)
@@ -22,27 +24,8 @@ test("Should get the discnumber if it has one", async () => {
     .get("/tags")
     .attach("music", "tests/fixtures/disc.mp3")
     .expect(200);
-  expect(response.body.disc).toEqual("2");
+  expect(response.body.disk).toEqual(2);
 });
-
-const TagsUtil = require("../src/utils/tagsutil");
-
-const testTags = {
-  title: "bob title",
-  artist: "bob artist",
-  album: "bob album",
-  comment: { text: "bob comment" },
-  track: "1/19",
-  genre: "20",
-  TPOS: { data: "2/9" },
-  TDRC: { data: "2005" },
-  TBPM: { data: "123" },
-  TKEY: { data: "7A" }
-};
-
-const getTestTags = () => {
-  return JSON.parse(JSON.stringify(testTags));
-};
 
 test("Should get tags from testTags", async () => {
   const tags = TagsUtil.extract(getTestTags());
@@ -51,47 +34,12 @@ test("Should get tags from testTags", async () => {
     artist: "bob artist",
     album: "bob album",
     comment: "bob comment",
-    track: "1",
+    track: 3,
     genre: "Alternative",
-    disc: "2",
-    year: "2005",
-    BPM: "123",
-    KEY: "7A"
+    disk: 2,
+    year: 2005,
+    bpm: 123,
+    key: "7A",
+    duration: 234.56
   });
-});
-
-test("Should get string comment", async () => {
-  let localTestTags = getTestTags();
-  localTestTags.comment = "new bob comment";
-  const tags = TagsUtil.extract(localTestTags);
-  expect(tags.comment).toEqual("new bob comment");
-});
-
-test("Should get year from year id3 tag", async () => {
-  let localTestTags = getTestTags();
-  delete localTestTags.TDRC;
-  localTestTags.year = "1997";
-  const tags = TagsUtil.extract(localTestTags);
-  expect(tags.year).toEqual("1997");
-});
-
-test("Should not include year in tags without year or TDRC id3 tag", async () => {
-  let localTestTags = getTestTags();
-  delete localTestTags.TDRC;
-  const tags = TagsUtil.extract(localTestTags);
-  expect(tags.year).toBeUndefined();
-});
-
-test("Should not include BPM in tags without TBPM id3 tag", async () => {
-  let localTestTags = getTestTags();
-  delete localTestTags.TBPM;
-  const tags = TagsUtil.extract(localTestTags);
-  expect(tags.BPM).toBeUndefined();
-});
-
-test("Should not include KEY in tags without TKEY id3 tag", async () => {
-  let localTestTags = getTestTags();
-  delete localTestTags.TKEY;
-  const tags = TagsUtil.extract(localTestTags);
-  expect(tags.KEY).toBeUndefined();
 });
