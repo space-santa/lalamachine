eb8ed0dfa6c01023e7ba7ef4c3c435e7e6bd6c93
space-santa <claus.z@protonmail.com>
2020-01-04 09:57:12 +1100

    Add tests for album

--- TagReaderService/tests/musiclib.test.js
+++ TagReaderService/tests/musiclib.test.js
@@ -19,6 +19,24 @@ const genreList = [
   { _id: "507f191e810c19729de860ff", name: "Alternative" }
 ];
 
+const albumList = [
+  {
+    _id: "507f191e810c19729de8aaea",
+    name: "Black Album",
+    artist: { _id: "507f191e810c19729de860ea", name: "Bobo" }
+  },
+  {
+    _id: "507f191e810c19729de860ea",
+    name: "White Album",
+    artist: { _id: "507f191e810c19729de860ea", name: "Joe" }
+  },
+  {
+    _id: "507f191e810c19729de860ff",
+    name: "Green Red",
+    artist: { _id: "507f191e810c19729de860ea", name: "Johnny" }
+  }
+];
+
 const findNameMatch = (query, list) => {
   const values = [];
 
@@ -113,6 +131,51 @@ test("Should find no genre by name", async () => {
   mockingoose(Genre).toReturn(finderMock, "find");
   const response = await request(app)
     .get("/genres")
+    .query({ name: "Jim" })
+    .expect(200);
+
+  expect(response.body).toEqual([]);
+});
+
+test("Should get all albums", async () => {
+  const finderMock = query => {
+    return findNameMatch(query, albumList);
+  };
+
+  Album.schema.path("artist", Object);
+  mockingoose(Album).toReturn(finderMock, "find");
+  const response = await request(app)
+    .get("/albums")
+    .expect(200);
+
+  expect(response.body).toEqual(albumList);
+});
+
+test("Should get albums by name", async () => {
+  const finderMock = query => {
+    return findNameMatch(query, albumList);
+  };
+
+  Album.schema.path("artist", Object);
+  mockingoose(Album).toReturn(finderMock, "find");
+  const response = await request(app)
+    .get("/albums")
+    .query({ name: "lbum" })
+    .expect(200);
+
+  expect(response.body[0].artist.name).toEqual("Bobo");
+  expect(response.body[1].artist.name).toEqual("Joe");
+});
+
+test("Should find no album by name", async () => {
+  const finderMock = query => {
+    return findNameMatch(query, albumList);
+  };
+
+  Album.schema.path("artist", Object);
+  mockingoose(Album).toReturn(finderMock, "find");
+  const response = await request(app)
+    .get("/albums")
     .query({ name: "Jim" })
     .expect(200);
 
