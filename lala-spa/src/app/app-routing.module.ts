2931d6c2e3c6fe0a12d83f0ae7eff2f2dddd787f
claus <claus.z@protonmail.com>
2020-01-30 20:45:06 +1100

    Add initial app functionality

--- lala-spa/src/app/app-routing.module.ts
+++ lala-spa/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { MainPlaylistComponent } from "./main-playlist/main-playlist.component";
+import { LibraryComponent } from "./library/library.component";
+import { SettingsComponent } from "./settings/settings.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: MainPlaylistComponent },
+  { path: "library", component: LibraryComponent },
+  { path: "settings", component: SettingsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
