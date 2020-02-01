d4af5cfa4435ceee53e0311f36a2508a09919f02
claus <claus.z@protonmail.com>
2020-02-02 09:21:36 +1100

    Don't hardcode the absolute path to the dummy file

--- lala-server/src/files/files.controller.ts
+++ lala-server/src/files/files.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get, Param, ParseIntPipe, Res } from "@nestjs/common";
+import * as path from "path";
 
 @Controller("files")
 export class FilesController {
   @Get("/:id")
   getFileById(@Param("id", ParseIntPipe) id: number, @Res() res): Buffer {
     return res.sendFile(
-      "D:/code/lalamachine/lala-server/test/fixtures/good.mp3"
+      path.resolve(__dirname + "../../../test/fixtures/good.mp3")
     );
   }
 }
