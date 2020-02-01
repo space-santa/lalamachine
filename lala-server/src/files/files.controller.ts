42fe9494e53a819fb4e76eb6c8466a9521387b8f
space-santa <claus.z@protonmail.com>
2020-02-01 21:51:56 +1100

    Add dummy files controller

--- lala-server/src/files/files.controller.ts
+++ lala-server/src/files/files.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Param, ParseIntPipe, Res } from "@nestjs/common";
 
 @Controller("files")
-export class FilesController {}
+export class FilesController {
+  @Get("/:id")
+  getFileById(@Param("id", ParseIntPipe) id: number, @Res() res): Buffer {
+    return res.sendFile(
+      "D:/code/lalamachine/lala-server/test/fixtures/good.mp3"
+    );
+  }
+}
